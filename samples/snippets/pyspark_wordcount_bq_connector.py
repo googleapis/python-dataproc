5cc9f3cf17f72b50f86f4235a073fd381aa200f8
Leah Cole <coleleah@google.com>
2021-12-10 13:17:31 -0800

    initial commit for pyspark wordcount sample

---
+++ samples/snippets/pyspark_wordcount_bq_connector.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""BigQuery I/O PySpark example."""
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+  .builder \
+  .master('yarn') \
+  .appName('spark-bigquery-demo') \
+  .getOrCreate()
+
+# Use the Cloud Storage bucket for temporary BigQuery export data used
+# by the connector.
+bucket = "test-bucket"
+spark.conf.set('temporaryGcsBucket', bucket)
+
+# Load data from BigQuery.
+words = spark.read.format('bigquery') \
+  .option('table', 'bigquery-public-data:samples.shakespeare') \
+  .load()
+words.createOrReplaceTempView('words')
+
+# Perform word count.
+word_count = spark.sql(
+    'SELECT word, SUM(word_count) AS word_count FROM words GROUP BY word')
+word_count.show()
+word_count.printSchema()
+
+# Saving the data to BigQuery
+word_count.write.format('bigquery') \
+  .option('table', 'wordcount_dataset.wordcount_output') \
+  .save()
