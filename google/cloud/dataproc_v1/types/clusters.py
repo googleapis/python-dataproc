0f94eb20e6df8d4f97e8d2a6aff39bfb7a5aa92c
Tang Quoc Thai <victortang212@gmail.com>
2022-12-16 11:38:04 +0000

    feat: add SPOT to Preemptibility options

--- google/cloud/dataproc_v1/types/clusters.py
+++ google/cloud/dataproc_v1/types/clusters.py
@@ -779,10 +779,30 @@ class InstanceGroupConfig(proto.Message):
         r"""Controls the use of [preemptible instances]
         (https://cloud.google.com/compute/docs/instances/preemptible) within
         the group.
+
+        Values:
+            PREEMPTIBILITY_UNSPECIFIED: Preemptibility is unspecified, the system
+                will choose the appropriate setting for each instance group.
+            NON_PREEMPTIBLE: Instances are non-preemptible.This option is allowed
+                for all instance groups and is the only valid value for Master and
+                Worker instance groups.
+            PREEMPTIBLE: Instances are preemptible
+                (https://cloud.google.com/compute/docs/instances/preemptible).This
+                option is allowed only for secondary worker
+                (https://cloud.google.com/dataproc/docs/concepts/compute/secondary-
+                vms) groups.
+            SPOT: Instances are Spot VMs
+                (https://cloud.google.com/compute/docs/instances/spot).This option is
+                allowed only for secondary worker
+                (https://cloud.google.com/dataproc/docs/concepts/compute/secondary-
+                vms) groups. Spot VMs are the latest version of preemptible VMs
+                (https://cloud.google.com/compute/docs/instances/preemptible), and
+                provide additional features.
         """
         PREEMPTIBILITY_UNSPECIFIED = 0
         NON_PREEMPTIBLE = 1
         PREEMPTIBLE = 2
+        SPOT = 3
 
     num_instances = proto.Field(
         proto.INT32,
